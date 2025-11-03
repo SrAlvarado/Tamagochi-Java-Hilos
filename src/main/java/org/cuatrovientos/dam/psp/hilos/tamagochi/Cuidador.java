44f3ada0cb1d6bcefba48ae8a19b73fcba3d2064
Markel Alvarado <markel.alvarado@gmail.com>
2025-11-03 08:38:53 +0100

    Creacion de proyecto Tamagochi

---
+++ src/main/java/org/cuatrovientos/dam/psp/hilos/tamagochi/Cuidador.java
@@ -0,0 +1,13 @@
+package org.cuatrovientos.dam.psp.hilos.tamagochi;
+
+/**
+ * Hello world!
+ *
+ */
+public class Cuidador
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
